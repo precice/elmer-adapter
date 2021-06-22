6416bebf1d9b105a391dc1a575feb4eb835581c3
hisham <ge49not@mytum.de>
2021-06-22 11:35:40 +0200

    organised code

---
+++ Partitioned_Heat_Conduction/Coupler_Solver.F90
@@ -0,0 +1,264 @@
+MODULE HelperMethods
+
+    !------------------------------------------------------------------------------
+    USE DefUtils
+    !------------------------------------------------------------------------------
+    IMPLICIT NONE
+
+    
+
+    CONTAINS
+
+    SUBROUTINE Print(dataName,mesh,BCPerm,CoordVals)
+
+        !-------------------------Strings----------------------------------------------
+        CHARACTER(LEN=MAX_NAME_LEN)         :: dataName
+        CHARACTER(LEN=MAX_NAME_LEN)         :: infoMessage
+        !-------------------------Elmer_Types----------------------------------------------
+        TYPE(Variable_t), POINTER           :: dataVariable
+        TYPE(Mesh_t), POINTER               :: mesh
+        !------------------------Data Arrays----------------------------------------------
+        REAL(KIND=dp), POINTER              :: CoordVals(:)
+        INTEGER, POINTER                    :: BCPerm(:)
+        !--------------------------Iterators-------------------------------------
+        INTEGER                             :: i,j
+    
+        dataVariable  => VariableGet( mesh % Variables, dataName)
+
+        CALL Info('CouplerSolver','Printing ' //TRIM(dataName))
+        DO i = 1, mesh % NumberOfNodes
+            j = BCPerm(i)
+            IF(j == 0) CYCLE
+            
+            write(infoMessage,'(A,I5,A,I5,A,F10.4,A,F10.2,A,F10.2)') 'Node: ',i,' Index: ',j,' Value: ' &
+                            ,dataVariable % Values(dataVariable % Perm(i)),&
+                            ' X= ', CoordVals(3*j-2), ' Y= ', CoordVals(3*j-1)  
+                                                          
+            CALL Info('CouplerSolver',infoMessage)
+
+        END DO 
+    
+        
+    END SUBROUTINE Print
+
+    SUBROUTINE CreateVariable(dataName,dataType,mesh,BCPerm,Solver,solverParams)
+        !-------------------------Strings-----------------------------------------------
+        CHARACTER(LEN=MAX_NAME_LEN)         :: dataName
+        CHARACTER(LEN=MAX_NAME_LEN)         :: infoMessage
+        character(len=*), intent(in) :: dataType
+        !-------------------------Elmer_Types-------------------------------------------
+        TYPE(Variable_t), POINTER           :: dataVariable
+        TYPE(Mesh_t), POINTER               :: mesh
+        TYPE(Solver_t)                      :: Solver
+        TYPE(ValueList_t), POINTER          :: solverParams
+        !------------------------Data Arrays--------------------------------------------
+        INTEGER, POINTER                    :: BCPerm(:)
+        !------------------------Mesh Data----------------------------------------------
+        INTEGER                         :: Dofs
+        !--------------------------Logic-Control-------------------------------------
+        LOGICAL                             :: Found
+
+        
+        dataName =  ListGetString(solverParams,dataType,Found )
+        dataVariable  => VariableGet( mesh % Variables, dataName)
+        IF(ASSOCIATED( dataVariable ) ) THEN
+            CALL Info('CouplerSolver','Using existing variable : '//TRIM(dataName) )
+        ELSE
+            CALL Info('CouplerSolver','Creating variable  as it does not exist: '//TRIM(dataName))
+
+            Dofs = ListGetInteger( solverParams,'Field Dofs',Found )
+            IF(.NOT. Found ) Dofs = 1
+            CALL VariableAddVector( mesh % Variables, mesh, Solver, dataName, Dofs, &
+                Perm = BCPerm, Secondary = .TRUE. )
+            dataVariable => VariableGet( mesh % Variables, dataName ) 
+        END IF
+
+
+    END SUBROUTINE CreateVariable
+    
+
+END MODULE HelperMethods
+
+
+
+SUBROUTINE CouplerSolver( Model,Solver,dt,TransientSimulation)
+
+    !------------------------------------------------------------------------------
+    USE DefUtils
+    USE HelperMethods
+    !------------------------------------------------------------------------------
+    IMPLICIT NONE
+    !--------------------------UDS Prerequistes------------------------------------
+    TYPE(Solver_t) :: Solver
+    TYPE(Model_t) :: Model
+    REAL(KIND=dp) :: dt
+    LOGICAL :: TransientSimulation
+
+
+    !--------------------------Variables-Start-------------------------------------------
+    
+    !--------------------------MPI-Variables-------------------------------------
+    INTEGER                         :: rank,commsize
+    !--------------------------Precice-Control-------------------------------------
+    INTEGER                         :: itask = 1
+
+    !--------------------------Logic-Control-------------------------------------
+    LOGICAL                             :: Found
+    !--------------------------Iterators-------------------------------------
+    INTEGER                             :: i,j
+
+
+    !--------------------------Elmer-Variables-------------------------------------
+    !-------------------------Strings----------------------------------------------
+    CHARACTER(LEN=MAX_NAME_LEN)         :: maskName
+    CHARACTER(LEN=MAX_NAME_LEN)         :: infoMessage
+    !-------------------------Elmer_Types----------------------------------------------
+    TYPE(Variable_t), POINTER           :: readDataVariable,writeDataVariable
+    TYPE(Mesh_t), POINTER               :: mesh
+    TYPE(ValueList_t), POINTER          :: simulation, solverParams ! Simulation gets Simulation list, & solverParams hold solver1,solver 2,etc
+    !------------------------Data Arrays----------------------------------------------
+    REAL(KIND=dp), POINTER              :: CoordVals(:)
+    INTEGER, POINTER                    :: BCPerm(:)
+    !------------------------Time Variable----------------------------------------------
+    TYPE(Variable_t), POINTER :: TimeVar
+    Real(KIND=dp) :: Time
+
+    !--------------------------Precice-Variables-------------------------------------
+    !-------------------------Strings----------------------------------------------
+    CHARACTER(LEN=MAX_NAME_LEN)         :: participantName, meshName, configPath, readDataName,writeDataName
+    CHARACTER(LEN=MAX_NAME_LEN)         :: writeInitialData, readItCheckp, writeItCheckp ! ?? Do not know
+    !-------------------------IDs-Integer----------------------------------------------
+    INTEGER                         :: meshID,readDataID, writeDataID
+    !------------------------Data Arrays----------------------------------------------
+    REAL(KIND=dp), POINTER :: writeData(:), readData(:)
+    INTEGER, POINTER                :: vertexIDs(:)
+    !------------------------Mesh Data----------------------------------------------
+    INTEGER                         :: vertexSize
+    INTEGER                         :: Dofs
+    INTEGER                         :: dimensions ! ?? Do not know 
+    !----------------------Time Loop Control Variables-----------------------------
+    INTEGER                         :: bool
+    INTEGER                         :: ongoing
+    !--------------------------Variables-End-------------------------------------------
+
+    !--------------------------SAVE-Start-------------------------------------------
+    SAVE meshID,readDataID,writeDataID
+    SAVE meshName,readDataName,writeDataName
+    SAVE itask
+    SAVE BCPerm,CoordVals,vertexIDs
+    SAVE readData,writeData
+    SAVE vertexSize
+
+    !--------------------------SAVE-End-------------------------------------------
+
+    !--------------------------Initialize-Start-------------------------------------------
+    CALL Info(''//achar(27)//'[31mCouplerSolver ', 'Transfering results between different software ')
+
+    Simulation => GetSimulation()
+    Mesh => Solver % Mesh
+    solverParams => GetSolverParams()
+
+    rank = 0
+    commsize = 1
+    !--------------------------Initialize-End-------------------------------------------
+
+    
+
+    select case(itask)
+        ! TODO make enum
+    case(1)
+        !--- First Time Visited, Initialization
+        !-- First Time Visit, Create Precice, create nodes at interface
+        !----------------------------- Initialize---------------------
+        !----------------Acquire Names for solver---------------------
+        maskName = GetString( Simulation, 'maskName', Found )
+        participantName = GetString( Simulation, 'participantName', Found )
+        meshName = GetString( Simulation, 'meshName', Found )
+        configPath = GetString( Simulation, 'configPath', Found )
+
+        Print *, TRIM(maskName)," ",TRIM(participantName)," ",TRIM(meshName)," ",TRIM(configPath)
+        
+
+        !-----------Identify Vertex on Coupling Interface & Save Coordinates--------------------
+        NULLIFY( BCPerm )    
+        ALLOCATE( BCPerm( Mesh % NumberOfNodes ) )
+        BCPerm = 0
+        ! CALL MakePermUsingMask( Model, Solver, Mesh, MaskName, .FALSE., &
+        !     BCPerm, vertexSize )
+        CALL MakePermUsingMask( Model, Solver, Mesh, MaskName, .TRUE., &
+            BCPerm, vertexSize )
+        CALL Info('CouplerSolver','Number of nodes at interface:'//TRIM(I2S(vertexSize)))
+
+        ALLOCATE( CoordVals(3*vertexSize) )
+        ALLOCATE(vertexIDs(vertexSize)) 
+        DO i=1,Mesh % NumberOfNodes
+            j = BCPerm(i)
+            CoordVals(3*j-2) = mesh % Nodes % x(i)
+            CoordVals(3*j-1) = mesh % Nodes % y(i)
+            CoordVals(3*j) = mesh % Nodes % z(i)
+            IF(j /= 0) THEN
+                vertexIDs(j) = j
+            END IF
+        END DO
+        CALL Info('CouplerSolver','Created nodes at interface')   
+        
+        !-----------Identify read Variables and Create it if it does not exist--------------------
+        CALL CreateVariable(readDataName,'readDataName',mesh,BCPerm,Solver,solverParams)
+
+        !-----------Print Read Values, For Debugging Purposes--------------------
+        CALL Info('CouplerSolver','Printing read Data')
+        CALL Print(readDataName,mesh ,BCPerm,CoordVals)
+        !------------------------------------------------------------------------------
+       
+        !-----------Identify write Variables and Create it if it does not exist--------------------
+        CALL CreateVariable(writeDataName,'writeDataName',mesh,BCPerm,Solver,solverParams)
+
+        !-----------Print Write Values, For Debugging Purposes--------------------
+        CALL Info('CouplerSolver','Printing write Data')
+        CALL Print(writeDataName,mesh ,BCPerm,CoordVals)
+
+        !------------------------------------------------------------------------------
+
+        itask = 2
+    case(2)
+        CALL Info('CouplerSolver','Printing read Data')
+        CALL Print(readDataName,mesh ,BCPerm,CoordVals)
+        
+
+        CALL Info('CouplerSolver','Printing write Data')
+        CALL Print(writeDataName,mesh ,BCPerm,CoordVals)
+        
+
+        itask = 3
+    case(3)
+        !-------------------Copy Write values from Variable to buffer---------------------
+        
+        
+        CALL Info('CouplerSolver','Printing write Data')
+        CALL Print(writeDataName,mesh ,BCPerm,CoordVals)
+        
+
+        CALL Info('CouplerSolver','Printing read Data')
+        CALL Print(readDataName,mesh ,BCPerm,CoordVals)
+
+        itask = 2
+    case(4)    
+    end select
+
+    
+
+    
+
+        
+
+
+    CALL Info('CouplerSolver',' Ended '//achar(27)//'[0m.')
+END SUBROUTINE CouplerSolver
+
+
+
+
+
+
+
+
