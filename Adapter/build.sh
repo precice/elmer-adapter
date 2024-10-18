65946c746221097b6879e9bbecd3c3f5c58af6f0
Ishaan Desai <ishaandesai@gmail.com>
2024-10-18 14:36:00 +0200

    Link to libprecice.so.3 in the adapter build.sh

--- Adapter/build.sh
+++ Adapter/build.sh
@@ -1,3 +1,3 @@
-elmerf90 -o Coupler_Solver.so Coupler_Solver.F90 /usr/lib/x86_64-linux-gnu/libprecice.so.2
-elmerf90 -o Print_Module.so Print_Module.F90 /usr/lib/x86_64-linux-gnu/libprecice.so.2
-elmerf90 -o UDF_Boundary.so UDF_Boundary.F90 /usr/lib/x86_64-linux-gnu/libprecice.so.2
+elmerf90 -o Coupler_Solver.so Coupler_Solver.F90 /usr/lib/x86_64-linux-gnu/libprecice.so.3
+elmerf90 -o Print_Module.so Print_Module.F90 /usr/lib/x86_64-linux-gnu/libprecice.so.3
+elmerf90 -o UDF_Boundary.so UDF_Boundary.F90 /usr/lib/x86_64-linux-gnu/libprecice.so.3
